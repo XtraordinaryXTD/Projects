8c907610636fe6986af9f97f61bace56541e6d03
XtraordinaryXTD <94047498+XtraordinaryXTD@users.noreply.github.com>
2022-11-01 14:28:17 +0100

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,5 +1,6 @@
 """Hangman"""
 import random
+from turtle import position
 
 
 word_list = ["aardvark", "baboon", "camel"]
@@ -13,21 +14,16 @@ display = []
 
 word_length = len(chosen_word)
 
-for _ in range(word_length):
+for blank in range(word_length):
     display += "_"
 
 
-# #TODO-2: - Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-for letter in chosen_word:
+for position in range(word_length):
+    letter = chosen_word[position]
     if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+        display[position] = letter
 
-#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+
 
 
 print(display)
