da2c25f43f47a1e5a49843900274014acd85c5a7
XtraordinaryXTD <94047498+XtraordinaryXTD@users.noreply.github.com>
2022-11-03 08:41:02 +0100

    Add files via upload

--- Hangman.py
+++ Hangman.py
@@ -1,31 +1,121 @@
+
 """Hangman"""
 import random
 from turtle import position
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
+# Constants
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-
-print(f'Hey the chosen word is {chosen_word}')
-
-guess = input('Choose a letter: ').lower()
-
-display = []
-
 word_length = len(chosen_word)
+display = []
+lives_stages = stages
+lives = 6
+game_over = False
+
+# print(f'Hey the chosen word is {chosen_word}')
+
 
 for blank in range(word_length):
     display += "_"
 
-
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
-
-
-
-
 print(display)
+
+
+if lives == 6:
+    print(stages[-1])
+
+
+while not game_over:
+    guess = input('Choose a letter: ').lower()
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter    
+      
+    
+    if guess not in chosen_word:
+        lives_left = lives_stages[lives]
+        lives -= 1
+        print(lives_left)
+
+        if lives == 0:
+            game_over = True
+            print(lives_stages[0])
+            print('Game Over!!!')
+        
+    # print(f'{lives} lives left')
+    
+    print(display)
+
+    if "_" not in display:
+        game_over = True
+        print('You WON!!!')
+    
+
+
+
+
+
+
+
+
 
 
